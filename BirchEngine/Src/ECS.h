e1119762b1e0ca9375313f18026718d82ae08763
ethansam911 <esam5556@gmail.com>
2019-09-29 12:01:32 -0400

    update

--- BirchEngine/Src/ECS.h
+++ BirchEngine/Src/ECS.h
@@ -16,7 +16,7 @@ inline ComponentID getComponentID()
 	return lastID++;
 }
 
-template <typename T> inline ComponentID getComponentID() noexcept
+template <typename T> inline ComponentID getComponentTypeID() noexcept
 {
 	static ComponentID typeID = getComponentID();
 	return typeID();
@@ -82,6 +82,56 @@ public:
 	}
 	
 	template <typename T, typename... TArgs>
+	T& addComponent(TArgs&& ... mArgs)
+	{
+		T* c(new T(std::forward<TArgs>(mArgs)...))
+		c->entity = this;
+		std::unique_ptr<Component> uPtr{ c };
+		components.emplace_back(std::move(uPtr));
 
+		componentArray[getComponentTypeID<T>()]= c;
+		componentBitSet[getComponentTypeID<T>()] = true;
+
+		c->init();
+		return *c;
+	}
+
+	template<typename T> T& getComponent() const
+	{
+		auto ptr(ComponentArray[getComponentTypeID<T>()]);
+		return *static_cast<T*>(ptr);
+	}
 
 };
+
+class Manager
+{
+private:
+	std::vector<std::unique_ptr<Entity>> entities;
+
+public:
+
+	void update()
+	{
+		for (auto& e : entities) e->update();
+	}
+	void draw()
+	{
+		for (auto& e : entities) e->draw();
+	}
+	void refresh()
+	{
+		entities.erase(std::remove_if(std::begin(entities), std::end(entities),
+			[](const std::unique_ptr<Entity>& mEntity)
+			{
+				return !mEntity->isActive();
+			}},
+				std::end(entities));	
+	}
+	Entity& addEntity()
+	{
+		Entity* e = new Entity();
+		std::unique_ptr<Entity> uPtr{ e };
+
+
+	}
